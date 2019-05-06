beaa8ac39bbc5410e31ba1d82e32886163cfd54e
amazonics123 <49821094+amazonics123@users.noreply.github.com>
2019-05-06 00:42:54 -0500

    function that takes in sentence list and returns sentiments

--- nltk_trial.py
+++ nltk_trial.py
@@ -1,4 +1,5 @@
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 sentence = "At eight o'clock on Thursday morning, Arthur didn't feel very good."
 tokens = nltk.word_tokenize(sentence)
@@ -7,3 +8,15 @@ tagged = nltk.pos_tag(tokens)
 print(tagged)
 entities = nltk.chunk.ne_chunk(tagged)
 print(entities)
+
+def analyze_sentences():
+    '''
+    Takes in a LIST of sentences and spits out some cool stuff.
+    '''
+sid = SentimentIntensityAnalyzer()
+for sentence in sentences:
+    print(sentence)
+    ss = sid.polarity_scores(sentence)
+    for k in sorted(ss):
+        print('{0}: {1}, '.format(k, ss[k]), end='')
+    print()
