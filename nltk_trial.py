7baf77e7c81f5ca308b3b19dbc99f7c8c0395f51
Max Marcussen <maxmarcussen98@gmail.com>
2019-05-14 21:13:17 -0500

    made functions for analyzing sentences and getting entities

--- nltk_trial.py
+++ nltk_trial.py
@@ -1,22 +1,20 @@
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-sentence = "At eight o'clock on Thursday morning, Arthur didn't feel very good."
-tokens = nltk.word_tokenize(sentence)
-print(tokens)
-tagged = nltk.pos_tag(tokens)
-print(tagged)
-entities = nltk.chunk.ne_chunk(tagged)
-print(entities)
-
-def analyze_sentences(sentences):
+def analyze_sentences(sentence):
     '''
-    Takes in a LIST of sentences and spits out some cool stuff.
+    Takes in a string and spits out a dict of sentiments - compound, neg, pos, neu.
     '''
     sid = SentimentIntensityAnalyzer()
-    for sentence in sentences:
-        print(sentence)
-        ss = sid.polarity_scores(sentence)
-        for k in sorted(ss):
-            print('{0}: {1}, '.format(k, ss[k]), end='')
-        print()
+    print(sentence)
+    ss = sid.polarity_scores(sentence)
+    return (ss)
+
+def get_entities(sentence):
+    '''
+    gets the entities from the sentence.
+    '''
+    tokens = nltk.word_tokenize(sentence)
+    tagged = nltk.pos_tag(tokens)
+    entities = nltk.chunk.ne_chunk(tagged)
+    return entities
