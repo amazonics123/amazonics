646c3a4ac90c3cf9aa2e30dfa0079cf98b147594
Max Marcussen <maxmarcussen98@gmail.com>
2019-06-02 17:35:30 -0500

    gonna edit on my git

--- reduce_regression.py
+++ reduce_regression.py
@@ -2,6 +2,7 @@ from mrjob.job import MRJob
 import numpy as np
 import regression
 import pandas as pd
+import heapq
 
 class toplines(MRJob):
     
@@ -19,16 +20,24 @@ class toplines(MRJob):
         yield (arr, arrmatey), score
 
     def combiner_init(self):
+        '''
         score_store = []
         covars_store = []
+        '''
+        scores = {}
 
     def combiner(self, covars, score):
-        score_store.append(score)
-        covars_store.append(covars)
+        scores[covars] = score
 
     def combiner_final(self):
+        '''
         dataframe = pd.DataFrame(np.array([score_store, covars_store]), columns=['scores', 'covars'])
         yield None, dataframe.sort_values(by=['scores']).head(50)
+        '''
+        h = [(score, covars) for (covars, score) in list(counts.items())[:k]]
+        heapq.heapify(h)
+
+        for covars, score in
 
     def reducer_init(self):
         over_df = pd.DataFrame(columns=['scores', 'covars'])
