0e0aab8f83b08105f300379ce3e4bbdda2ae4248
Max Marcussen <maxmarcussen98@gmail.com>
2019-06-02 12:41:51 -0500

    this is something - it should work except for the lines thing and the double mapper

--- reduce_regression.py
+++ reduce_regression.py
@@ -1,32 +1,48 @@
 from mrjob.job import MRJob
 import numpy as np
 import regression
+import pandas as pd
 
 class toplines(MRJob):
     
-    def mapper_init(self):
-        value_storer = []
-        added = False
-
-    def mapper(self, _, line):
+    def mapper_first(self, _, line):
         '''
         pass to another mapper? for this line, give another mapper
         the csv and the line and then compare them all
         '''
         arr = np.asarray(line.split())
-        if arr not in value_storer:
-            if not added:
-                value_storer.append(arr)
-                added = True
-            else:
-                score = regression.find_best_fit(arr, value_storer[-1])
-    	yield (arr, value_storer[-1]), score
+    	yield arr, None
 
-    def combiner():
-        yield
+    def mapper_second(self, arr, line):
+        arrmatey = np.asarray(line.split())
+        score = regression.find_best_fit(arr, value_storer[-1])
+        yield (arr, arrmatey), score
 
-    def reducer():
-        yield
+    def combiner_init(self):
+        score_store = []
+        covars_store = []
+
+    def combiner(self, covars, score):
+        score_store.append(score)
+        covars_store.append(covars)
+
+    def combiner_final(self):
+        dataframe = pd.DataFrame(np.array([score_store, covars_store]), columns=['scores', 'covars'])
+        dataframe = dataframe.sort_values(by=['scores'])
+        yield None, dataframe.head(50)
+
+    def reducer_init(self):
+        over_df = pd.DataFrame(columns=['scores', 'covars'])
+
+    def reducer(self, _, df):
+        over_df = over_df.append(df)
+
+    def reducer_final(self):
+    	yield None, over_df.sort_values(by=['scores']).head(50)
 
     def steps(self):
-        return []
+        return [
+            MRStep(mapper=self.mapper_first),
+            MRStep(mapper=self.mapper_second,
+            	combiner=self.combiner,
+            	reducer=self.reducer)]
