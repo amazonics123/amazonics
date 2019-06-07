fbd094ff838cba2955c3d3188694fd5db53cbfd2
Liam Puknys <lpuknys@cs.uchicago.edu>
2019-06-07 01:42:09 -0500

    made things faster and without numpy

--- regression.py
+++ regression.py
@@ -18,8 +18,12 @@ def get_score(x, y):
             break
     working_x = x[start:]
     working_y = y[start:]
-    return np.corrcoef(working_x, working_y)[0][1] * \
-        np.log(max(1, min(1000, sum(working_x)) * min(1000, sum(working_y))))
+    sumx = sum(working_x)
+    sumy = sum(working_y)
+    return covariance(working_x, working_y, sumx, sumy) * \
+        (min(2000, sumx) * min(2000, sumy))**(1/2)
+    #return np.corrcoef(working_x, working_y)[0][1] * \
+        #np.log(max(1, min(1000, sum(working_x)) * min(1000, sum(working_y))))
 
 
 
@@ -27,23 +31,29 @@ def find_best_fit(x, y):
     '''
     '''
 
+    if sum(x) <= 20 or sum(y) <= 20:
+        return (0, 0)
+
+    orig_len = len(x)
     best_score = 0
     offset = 0
-    for i in range(len(x)-1):
-        y_range = len(y) - i
+
+    for i in range(orig_len - 1):
+        y_range = orig_len - i
         x_slice = x[i:]
         y_slice = y[:y_range]
-        if sum(x_slice) == 0 or sum(y_slice) == 0:
+        if sum(x_slice) <= 20 or sum(y_slice) <= 20:
             break
         offset_score = get_score(x_slice, y_slice)
         if offset_score > best_score:
             best_score = offset_score
             offset = i
+
     for i in range(len(y)-1):
         x_range = len(x) - i
         x_slice = x[:x_range]
         y_slice = y[i:]
-        if sum(x_slice) == 0 or sum(y_slice) == 0:
+        if sum(x_slice) <= 20 or sum(y_slice) <= 20:
             break
         offset_score = get_score(y_slice, x_slice)
         if offset_score == 0:
@@ -70,3 +80,21 @@ def do_everything(line1, line2):
         y.append(float(line2[i]))
     best = find_best_fit(x,y)
     return((id1, id2, best[1]), best[0])
+
+
+
+
+
+def covariance(x, y, sumx, sumy):
+    '''
+    '''
+    N = len(x)
+    meanx = sumx/N
+    meany = sumy/N
+    cov = 0
+
+    for i in range(N):
+        cov += (x[i]-meanx)*(y[i]-meany)
+
+    cov = cov/(N-1)
+    return cov
