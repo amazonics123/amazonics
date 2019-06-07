b13dbe91728dcebc789ac0d788b96b606dd0d8a5
Max Marcussen <maxmarcussen98@gmail.com>
2019-06-07 06:48:58 -0500

    other reducer file

--- other_reduce.py
+++ other_reduce.py
@@ -1,7 +1,6 @@
 from mrjob.job import MRJob
 import heapq
 
-
 class toplines(MRJob):
     
 
@@ -128,6 +127,100 @@ def compare_lexicographic_order(str1, str2):
         if o1 < o2:
             return False
     return False
+
+def get_score(x, y):
+    '''
+    '''
+    
+    orig_len = len(x)
+    start = 0
+    for i in range(orig_len):
+        if x[i] != 0:
+            start = i
+            break
+    for j in range(orig_len):
+        if y[j] != 0:
+            start = max(j, start)
+            break
+    working_x = x[start:]
+    working_y = y[start:]
+    sumx = sum(working_x)
+    sumy = sum(working_y)
+    return covariance(working_x, working_y, sumx, sumy)
+    #return np.corrcoef(working_x, working_y)[0][1] * \
+        #np.log(max(1, min(1000, sum(working_x)) * min(1000, sum(working_y))))
+
+
+
+def find_best_fit(x, y):
+    '''
+    '''
+
+    orig_len = len(x)
+    best_score = 0
+    offset = 0
+
+    for i in range(orig_len - 1):
+        y_range = orig_len - i
+        x_slice = x[i:]
+        y_slice = y[:y_range]
+        if sum(x_slice) == 0 or sum(y_slice) == 0:
+            break
+        offset_score = get_score(x_slice, y_slice)
+        if offset_score > best_score:
+            best_score = offset_score
+            offset = i
+
+    for i in range(len(y)-1):
+        x_range = len(x) - i
+        x_slice = x[:x_range]
+        y_slice = y[i:]
+        if sum(x_slice) == 0 or sum(y_slice) == 0:
+            break
+        offset_score = get_score(y_slice, x_slice)
+        if offset_score > best_score:
+            best_score = offset_score
+            offset = -1 * i
+
+    return (best_score, offset)
+
+
+
+def do_everything(line1, line2):
+    '''
+    '''
+
+    id1 = line1[0]
+    id2 = line2[0]
+    x = []
+    y = []
+    for i in range(1, len(line1)):
+        x.append(float(line1[i]))
+    for i in range(1, len(line2)):
+        y.append(float(line2[i]))
+    best = find_best_fit(x,y)
+    return((id1, id2, best[1]), best[0])
+
+
+
+
+
+def covariance(x, y, sumx, sumy):
+    '''
+    '''
+    N = len(x)
+    meanx = sumx/N
+    meany = sumy/N
+    cov = 0
+
+    for i in range(N):
+        cov += (x[i]-meanx)*(y[i]-meany)
+    if cov > 1:
+        cov = cov/(N-1)
+        return cov
+    else:
+        cov = cov/N
+        return cov
 
 
 
